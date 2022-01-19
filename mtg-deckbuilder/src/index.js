ff12d794a33ef7e82fd0086ff45e853f9707639f
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-01-18 20:35:51 -0500

    GET request functionality added

--- mtg-deckbuilder/src/index.js
+++ mtg-deckbuilder/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 function Search(props) {
 	return (
 		<div>
-			<input type='input' value={props.searchValue} onChange={props.onChange} />
+			<input type='input' value={props.SearchBarValue} onChange={props.onChange} />
 			<button onClick={props.onClick}>Search</button>
 		</div>
 	);
@@ -40,33 +40,45 @@ class Page extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			SearchItem: null
+			SearchBarValue: "",
+			APIArray: null
 		};
 	};
 
-	handleChange() {
-
+	handleChange = (e) => {
+		this.setState({
+			SearchBarValue: this.state.SearchBarValue = e.target.value
+		});
 	};
 
-	handleClick() {
-
+	handleClick = () => {
+		this.apiCall();
 	};
 
 	renderSearchBar() {
 		return (
 			<Search
-				searchValue=""
+				SearchBarValue={this.state.SearchBarValue}
 				onChange={this.handleChange}
 				onClick={this.handleClick}
 			/>
 		);
 	};
 
-	render() {
-
+	apiCall() {
 		const searchString = "https://api.scryfall.com/cards/search?q=";
-		let queryString = "";
+		let queryString = searchString + this.state.SearchBarValue;
+		fetch(queryString).then(
+			results => results.json()).then(
+				(json) => {
+					this.setState({
+						APIArray: json
+					});
+				}
+			)
+	}
 
+	render() {
 		return (
 			<div>
 				{this.renderSearchBar()}
