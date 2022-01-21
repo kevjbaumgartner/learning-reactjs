5972b520c6de2ef5b44bae2fc89c9fadeee7fed8
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-01-20 21:25:24 -0500

    Refactored parsing
    Made it so the DisplayedArray was broken up into two parts, virtual and physical.
    Issue with key duplicates resolved

--- mtg-deckbuilder/src/index.js
+++ mtg-deckbuilder/src/index.js
@@ -37,7 +37,7 @@ function Card(props) {
 function Grid(props) {
 	return (
 		<div id="grid">
-			{props.DisplayedArray}
+			{props.PhysicalDisplayedArray}
 		</div>
 	);
 };
@@ -54,7 +54,8 @@ class Page extends React.Component {
 			ShowSaved: false, 		// 3. Boolean - true = show SavedArray <Card />s, false = show <Search /> bar and SearchedArray <Card />s
 			SearchedArray: null,	// 4a. Array - APIArray JSON parsed into <Card /> elements
 			SavedArray: [], 		// 4b. Array - saved <Card /> elements
-			DisplayedArray: null 	// 5. Array - holds the currently displayed <Card /> elements dependant on ShowSaved
+			VirtualDisplayedArray: null,
+			PhysicalDisplayedArray: null 	// 5. Array - holds the currently displayed <Card /> elements dependant on ShowSaved
 		};
 	};
 
@@ -75,19 +76,20 @@ class Page extends React.Component {
 	};
 
 	// <Grid /> <Card /> onClick handler
-	// Get the clicked <Card /> element from the SearchedArray and add it to the SavedArray
 	handleClick(i) {
-		console.log(i);
-		let selectedObject = this.state.SearchedArray[i];
-		console.log(selectedObject);
-		const tempArray = this.state.SavedArray;
-		tempArray.push(selectedObject);
+		if(this.state.ShowSaved) {
+			console.log(i);
+		} else { // Get the clicked <Card /> element from the SearchedArray and add it to the SavedArray
+			let selectedObject = this.state.VirtualDisplayedArray[i];
+			const tempArray = this.state.SavedArray;
+			tempArray.push(selectedObject);
 
-		this.setState({
-			SavedArray: tempArray
-		}, function () {
-			console.log(this.state.SavedArray);
-		});
+			this.setState({
+				SavedArray: tempArray
+			}, function () {
+				console.log(this.state.SavedArray);
+			});
+		};
 	};
 
 	// <Menu /> button onClick handler
@@ -95,7 +97,7 @@ class Page extends React.Component {
 		this.setState({
 			ShowSaved: (!this.state.ShowSaved ? true : false)
 		}, function (){
-			this.fillDisplayedArray();
+			this.fulfillDisplay();
 		});
 	};
 
@@ -126,7 +128,7 @@ class Page extends React.Component {
 	renderGrid() {
 		return (
 			<Grid
-				DisplayedArray={this.state.DisplayedArray}
+				PhysicalDisplayedArray={this.state.PhysicalDisplayedArray}
 			/>
 		);
 	};
@@ -147,7 +149,7 @@ class Page extends React.Component {
 					this.setState({
 						APIArray: json
 					}, function () {
-						this.parseResults();
+						this.parse();//Results();
 					});
 				}
 			)
@@ -156,65 +158,113 @@ class Page extends React.Component {
 		}
 	};
 
-	// Parse important data from the raw JSON return and store it in SearchedArray
-	parseResults() {
-		let unparsedArray = this.state.APIArray; // Create mutable version of the results
-		let SearchedArray = []; // local SearchedArray to be returned for the state SearchedArray
+	parse() {
+		const rawResults = this.state.APIArray;
+		let rawData = rawResults.data;
 
-		// Variables to parse results for
-		let cardName = "";
-		let cardImg = "";
-		let cardAlt = "";
+		let parsedCards = []; // name : data 
+		for (let i = 0; i < rawData.length; i++){
+			parsedCards.push({
+				name: rawData[i].name,
+				data: rawData[i]
+			})
+		}
 
-		// If the JSON returned actually has content
-		if (unparsedArray != null && unparsedArray.status != 400) {
-			console.log("Unparsed Array:")
-			console.log(unparsedArray);
-			for (let i = 0; i < unparsedArray.data.length; i++) {
-				try {
-					cardName = unparsedArray.data[i].name;
-					cardImg = unparsedArray.data[i].image_uris.normal;
-					cardAlt = cardName;
-				} catch (error) {
-					unparsedArray.data.splice(i, 1); // If any of the variables above cannot be parsed, remove that entry from the mutable array
-					i--; // Decrement to accomodate for the missing card
-					console.log(error);
-					continue;
-				}
-				console.log(unparsedArray.data[i]);
-				
-				SearchedArray.push(
-					<Card
-						key={"card-" + cardName}
-						cardName={cardName}
-						cardImg={cardImg}
-						cardAlt={cardAlt}
-						cardOnClick={() => this.handleClick(i)}
-					/>
-				);
-			};
-			console.log("Return Array:")
-			console.log(SearchedArray);
-
-			// Set the state SearchedArray to this local now-parsed SearchedArray
-			// Call fillDisplayedArray in the callback
-			this.setState({
-				SearchedArray: SearchedArray
-			}, function () {
-				this.fillDisplayedArray();
-			});
-		};
-	};
-
-	// Determine the state of the page using ShowSaved and fill the DisplayedArray with the respective <Card /> objects
-	fillDisplayedArray() {
-		const toBeDisplayedArray = this.state.ShowSaved ? this.state.SavedArray : this.state.SearchedArray;
 		this.setState({
-			DisplayedArray: toBeDisplayedArray
-		}, function () {
-			//console.log(this.state.DisplayedArray);
+			ParsedCards: parsedCards
+		}, function() {
+			this.fulfillDisplay();
 		});
 	};
+
+	fulfillDisplay() {
+		const parsedCards = this.state.ParsedCards;
+		const savedCards = this.state.SavedArray;
+		const ShowSaved = this.state.ShowSaved;
+
+		let desired = !ShowSaved ? parsedCards : savedCards;
+		let displayedArray = [];
+
+		for(let i = 0; i < desired.length; i++){
+			displayedArray.push(
+				<Card
+					key={"card-" + i + "-" + desired[i].name}
+					cardName={desired[i].name}
+					cardImg={desired[i].data.image_uris.normal != undefined ? desired[i].data.image_uris.normal : ""}
+					cardAlt={desired[i].name}
+					cardData={desired[i].data}
+					cardOnClick={() => this.handleClick(i)}
+				/>
+			)
+		}
+
+		this.setState({
+			VirtualDisplayedArray: desired,
+			PhysicalDisplayedArray: displayedArray
+		}, function () {
+			console.log(this.state.VirtualDisplayedArray);
+		});
+	};
+
+	// // Parse important data from the raw JSON return and store it in SearchedArray
+	// parseResults() {
+	// 	let unparsedArray = this.state.APIArray; // Create mutable version of the results
+	// 	let SearchedArray = []; // local SearchedArray to be returned for the state SearchedArray
+
+	// 	// Variables to parse results for
+	// 	let cardName = "";
+	// 	let cardImg = "";
+	// 	let cardAlt = "";
+
+	// 	// If the JSON returned actually has content
+	// 	if (unparsedArray != null && unparsedArray.status != 400) {
+	// 		console.log("Unparsed Array:")
+	// 		console.log(unparsedArray);
+	// 		for (let i = 0; i < unparsedArray.data.length; i++) {
+	// 			try {
+	// 				cardName = unparsedArray.data[i].name;
+	// 				cardImg = unparsedArray.data[i].image_uris.normal;
+	// 				cardAlt = cardName;
+	// 			} catch (error) {
+	// 				unparsedArray.data.splice(i, 1); // If any of the variables above cannot be parsed, remove that entry from the mutable array
+	// 				i--; // Decrement to accomodate for the missing card
+	// 				console.log(error);
+	// 				continue;
+	// 			}
+	// 			console.log(unparsedArray.data[i]);
+				
+	// 			SearchedArray.push(
+	// 				<Card
+	// 					key={"card-" + cardName}
+	// 					cardName={cardName}
+	// 					cardImg={cardImg}
+	// 					cardAlt={cardAlt}
+	// 					cardOnClick={() => this.handleClick(i)}
+	// 				/>
+	// 			);
+	// 		};
+	// 		console.log("Return Array:")
+	// 		console.log(SearchedArray);
+
+	// 		// Set the state SearchedArray to this local now-parsed SearchedArray
+	// 		// Call fillDisplayedArray in the callback
+	// 		this.setState({
+	// 			SearchedArray: SearchedArray
+	// 		}, function () {
+	// 			this.fillDisplayedArray();
+	// 		});
+	// 	};
+	// };
+
+	// // Determine the state of the page using ShowSaved and fill the DisplayedArray with the respective <Card /> objects
+	// fillDisplayedArray() {
+	// 	const toBeDisplayedArray = this.state.ShowSaved ? this.state.SavedArray : this.state.SearchedArray;
+	// 	this.setState({
+	// 		DisplayedArray: toBeDisplayedArray
+	// 	}, function () {
+	// 		//console.log(this.state.DisplayedArray);
+	// 	});
+	// };
 
 	render() {
 		return (
