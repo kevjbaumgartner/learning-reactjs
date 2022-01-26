0c7ce01987b91cbbfaf1c8bb5e294095b2c9aea3
Kevin Baumgartner <kevjbaumgartner@gmail.com>
2022-01-25 22:42:09 -0500

    More stylization & started multi-deck saving functionality

--- mtg-deckbuilder/src/index.js
+++ mtg-deckbuilder/src/index.js
@@ -7,28 +7,46 @@ import './index.css';
 
 // ******************** FUNCTION COMPONENTS ******************** //
 
-// <Menu /> rendered in <Page />
-function Menu(props) {
+// <Preview /> rendered in <Page />
+function Preview(props) {
 	return (
-		<div id="menu">
-			<img id='menuLogo' className='menuItem' src='' alt='logo' />
-			<button className={props.PageState == 0 ? 'menuItem selected' : 'menuItem'} onClick={props.changeToSearched}>Search</button>
-			<button className={props.PageState == 1 ? 'menuItem selected' : 'menuItem'} onClick={props.changeToSaved}>Saved</button>
+		<div id="preview">
+			{props.SavedArray}
+			Save deck as:
+			<input
+				type='field'
+			/>
+			<button>
+				Save
+			</button>
 		</div>
 	);
 };
 
-// <Search /> rendered in <Page />
+// <Menu /> rendered in <Page />
+function Menu(props) {
+	return (
+		<div id="menu">
+			<img id='menuLogo' className='menuItem' src='./logo.png' alt='logo' />
+			<div className={props.PageState == 0 ? 'menuButton selected' : 'menuButton'} onClick={props.changeToSearched}>{props.renderSearchBar()}</div>
+			<button className={props.PageState == 1 ? 'menuButton selected' : 'menuButton'} onClick={props.changeToSaved}>Saved Decks</button>
+		</div>
+	);
+};
+
+// <Search /> rendered in <Menu />
 function Search(props) {
 	return (
 		<div id="search">
+			Search
 			<input
 				type='field'
 				value={props.SearchBarValue}
 				onChange={props.onChange}
+				placeholder='Enter a card name!'
 			/>
 			<button onClick={props.onClick}>
-				Search
+				<i className="fas fa-search"></i>
 			</button>
 		</div>
 	);
@@ -64,11 +82,17 @@ class Page extends React.Component {
 		this.state = {					// State order:
 			SearchBarValue: "", 		// 1. String - value searched for using the API
 			APIArray: [], 				// 2. List - contains the exact JSON returned by the API
-			PageState: 0, 			// 3. Boolean - true = show SavedArray <Card />s, false = show <Search /> bar and ParsedArray <Card />s
+			PageState: 0, 				// 3. Int - Holds the current page state:
+			// 0 = Searched
+			// 1 = Saved
 			ParsedArray: [],			// 4a. Array - APIArray JSON parsed into a dictionary (name:data)
 			SavedArray: [], 			// 4b. Array - saved <Card /> elements
 			VirtualDisplayedArray: [],	// 5a. Array - holds the currently displayed card's dictionary values
-			PhysicalDisplayedArray: [] 	// 5b. Array - holds the currently displayed card's <Card /> elements
+			PhysicalDisplayedArray: [], // 5b. Array - holds the currently displayed card's <Card /> elements
+
+			PreviewArray: [],			//
+			DeckFocus: "unnamedDeck",	//
+			SavedDecks: []				//
 		};
 	};
 
@@ -117,6 +141,15 @@ class Page extends React.Component {
 
 	// ******************** RENDER (function component) METHODS ******************** //
 
+	// <Preview />
+	renderPreview() {
+		return (
+			<Preview
+				SavedArray={this.state.PhysicalDisplayedArray}
+			/>
+		);
+	};
+
 	// <Menu />
 	renderMenuBar() {
 		return (
@@ -125,6 +158,7 @@ class Page extends React.Component {
 				onClick={this.handlePageSwap}
 				changeToSearched={() => this.changeTo(0)}
 				changeToSaved={() => this.changeTo(1)}
+				renderSearchBar={() => this.renderSearchBar()}
 			/>
 		);
 	};
@@ -243,8 +277,11 @@ class Page extends React.Component {
 		return (
 			<div id='page'>
 				{this.renderMenuBar()}
-				{this.state.PageState == 0 ? this.renderSearchBar() : ""}
-				{this.renderGrid()}
+				<div id='display'>
+					{this.renderGrid()}
+					
+				</div>
+				{this.renderPreview()}
 			</div>
 		);
 	};
